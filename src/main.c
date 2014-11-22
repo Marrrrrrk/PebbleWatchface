c92a4e3611910e2118d90a0d1593007fd647ec26
Mark Broste <mbroste1@gmail.com>
2014-11-21 21:52:24 -0700

    Date layer
    Adding a date layer using system font gothic 24.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <pebble.h>
 static Window *s_main_window;
+static TextLayer *s_date_layer;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+
+static void update_date() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  static char buffer[] ="00/00/00";
+  strftime(buffer, sizeof("00/00/00"), "%D", tick_time);
+  text_layer_set_text(s_date_layer, buffer);
+}
+
 
 static void update_time() {
 	// temporary struct
@@ -28,6 +38,12 @@ static void update_time() {
 // Time handlers
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
+  update_date();
+}
+
+static void improve_text_layer(TextLayer *the_text_layer, GFont font) {
+  text_layer_set_font(the_text_layer, font);
+  text_layer_set_text_alignment(the_text_layer, GTextAlignmentCenter);
 }
 
 // Window Handlers
@@ -37,22 +53,31 @@ static void main_window_load(Window *window) {
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  //
+  
+  // date text layer
+  s_date_layer = text_layer_create(GRect(0, 0, 139, 50));
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  
+  // time text layer
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   
   // improve the view
+  GFont time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_VGA_48));
+  GFont date_font = fonts_get_system_font(FONT_KEY_GOTHIC_28);
+  improve_text_layer(s_date_layer, date_font);
+  improve_text_layer(s_time_layer, time_font);
   
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_VGA_48));
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
-  // add it as a child to the window root layer
+  // add our text layers
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
   
   // displays time upon load
   update_time();
+  update_date();
 
 }
 static void main_window_unload(Window *window) {
