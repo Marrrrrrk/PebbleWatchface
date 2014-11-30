a1074c634d3c8cf0e529a8be323f622b11a8de25
Mark Broste <mbroste1@gmail.com>
2014-11-30 16:24:57 -0700

    Weather js
    Javascript file to handle location acquisition and weather request.

---
+++ src/weather.js
@@ -0,0 +1,49 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // build url
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude + "&units=imperial";
+  
+  // make our call
+  xhrRequest(url, 'GET', function(responseText) {
+    var json = JSON.parse(responseText);
+    console.log("JSON response " + json.main.temp);
+    
+    var temperature = Math.round(json.main.temp);
+    console.log("Temperature is " + temperature);
+    
+    var conditions = json.weather[0].main;
+    console.log("Conditions are " + conditions);
+  });
+}
+
+function locationError(err) {
+  console.log("Error requesting location");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+  locationSuccess,
+  locationError,
+    {timeout:15000, maximumAge:6000}
+  );
+}
+
+
+// Listen for watchface open
+Pebble.addEventListener('ready', function(e) {
+  console.log("PebbleKit JS is ready");
+  getWeather();
+});
+
+// Listen for appmessage received
+Pebble.addEventListener('appmessage', function(e) {
+  console.log("AppMessage received!");
+});
